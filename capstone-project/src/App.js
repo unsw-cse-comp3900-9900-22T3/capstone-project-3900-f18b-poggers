d593d10beb19e018517f244fde699c4a0469b7a1
joenguyen0109 <joenguye0109@gmail.com>
2022-10-04 21:36:43 +1100

    work on show all recipe

--- capstone-project/src/App.js
+++ capstone-project/src/App.js
@@ -7,28 +7,35 @@ import awsconfig from './aws-exports';
 // import the API & graphqlOperation helpers as well as the query
 import { graphqlOperation } from 'aws-amplify'
 import { listRecipes } from './graphql/queries'
+import React from 'react'
+
 // configure amplify
 // this can be added in index.js as well
 Amplify.configure(awsconfig);
 Auth.configure(awsconfig);
 
+
+
 function App() {
 
-  async function listRecipes() {
-    API.graphql(graphqlOperation(listRecipes)).then((evt) => {
-            evt.data.listRecipes.items.map((document, i) => {
-            console.log("name: ", document.name)
-        });
-    });
+  const [data, setData] = React.useState({})
 
-  }
+  React.useEffect(() => {
+      async function fetchMyAPI() {
+        const apiData = await API.graphql({ query: listRecipes });
+        setData(apiData)
+        console.log(apiData)
+      }
+
+      fetchMyAPI()
+    }, [])
+
   const { signOut } = useAuthenticator()
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={listRecipes}>List Recipes</button>
         <button onClick={() => signOut()}>Log Out</button>
       </header>
     </div>
