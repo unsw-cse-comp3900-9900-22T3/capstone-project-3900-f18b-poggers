06fea5349426b650c238e832d09153ed748ef9b7
Matthew Lau <matthew.lau78@gmail.com>
2022-10-11 02:04:12 +1100

    added type declarations

---
+++ frontend/src/types/instacook-types.d.ts
@@ -0,0 +1,10 @@
+export type Recipe = {
+  id: string,
+  name: string,
+  content: string,
+  contributor: string,
+  fileImage: string,
+  createdAt: string,
+  updatedAt: string,
+  owner: string,
+}
