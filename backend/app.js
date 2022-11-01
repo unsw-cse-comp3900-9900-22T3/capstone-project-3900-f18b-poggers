b63feea72f314150db82c98d5fc9ef5db16af807
Raymond Chung <raymond.chung216@gmail.com>
2022-11-01 15:12:38 +1100

    CORS Fix

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,16 @@ app.use(bodyParser.json());
 
 app.use(isAuth);
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(
   "/graphql",
   graphqlHTTP({
@@ -22,6 +32,7 @@ app.use(
     graphiql: true,
   })
 );
+
 mongoose
   .connect(
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.hmp3jml.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
